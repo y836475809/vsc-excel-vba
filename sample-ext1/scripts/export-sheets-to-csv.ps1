437dee4ce2ab90b08eb59c4498e637917ea88d87
y836475809 <tttdaytttday@gmail.com>
2023-05-29 23:00:15 +0900

    change: csv出力スクリプト変更
    csv出力後、改行コードをcrlfに変更
    式のままで出力する

--- sample-ext1/scripts/export-sheets-to-csv.ps1
+++ sample-ext1/scripts/export-sheets-to-csv.ps1
@@ -8,18 +8,35 @@ try {
     $distdir = $Args[1]
 
     $bk = getWorkBooks $bookname $false
-    $bk.Application.displayAlerts = $false
+    $app = $bk.Application
+
+    $app.ScreenUpdating = $false
+    $app.DisplayStatusBar = $false
+    $app.EnableEvents = $false
+    # $app.Visible = $false
+
+    $app.displayAlerts = $false
     $orgsheet = $bk.ActiveSheet
     $bkPath =  $bk.FullName
     $null = New-Item $distdir -ItemType Directory -Force 
     foreach($ws in $bk.Worksheets){
         $ws.Activate()
+        $app.ActiveWindow.DisplayFormulas = $true
         $csvpath = Join-Path $distdir "$($ws.name).csv"
         $ws.SaveAs($csvpath, $fmtCSV)
+        $app.ActiveWindow.DisplayFormulas = $false
     }
+
+    $app.ScreenUpdating = $true
+    $app.DisplayStatusBar = $true
+    $app.EnableEvents = $true
+    # $app.Visible = $true
+
     $orgsheet.Activate()
     $bk.SaveAs($bkPath, $fmtXLSM)
 
+    Set-Location -Path $distdir
+    ls -file -filter *.csv | % { (get-content -encoding Default $_.FullName) -join "`r`n" | set-content -encoding Default $_.FullName }
 } catch {
     $jsonStr = getResJson "error" $_.Exception.Message ""
     Write-Output $jsonStr

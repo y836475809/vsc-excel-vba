ea492bc95c2813fa23193537d9a1c2c2cd786d2c
y836475809 <tttdaytttday@gmail.com>
2023-07-18 00:37:39 +0900

    change: VBA書き換え方法変更に対応してVBA定義変更
    以前は書き換え前後で文字数を一致させる必要があったが不要にな；つた

--- vsc-excel-vba/d.vb/f.d.vb
+++ vsc-excel-vba/d.vb/f.d.vb
@@ -1,9 +1,9 @@
 
 Public Module f
     ' Range
-    Public Function Ran(Cell1 As String, Optional Cell2 As String = "") As Range
+    Public Function Range(Cell1 As String, Optional Cell2 As String = "") As Range
     End Function
-    Public Function Ran(Cell1 As Range, Optional Cell2 As Range = Nothing) As Range
+    Public Function Range(Cell1 As Range, Optional Cell2 As Range = Nothing) As Range
     End Function
 
     Public Function Rows(Cell1 As String, Optional Cell2 As String = "") As Range
@@ -15,23 +15,23 @@ Public Module f
     End Function
 
     ' Cells
-    Public Function Cls(Optional RowIndex As Long, Optional ColumIndex As Long) As Range
+    Public Function Cells(Optional RowIndex As Long, Optional ColumIndex As Long) As Range
     End Function
 
     ' Workbooks
-    Public Function WorkBks() As Workbooks
+    Public Function Workbooks() As Workbooks
     End Function
-    Public Function WorkBks(name As String) As Workbook
+    Public Function Workbooks(name As String) As Workbook
     End Function
-    Public Function WorkBks(index As Long) As Workbook
+    Public Function Workbooks(index As Long) As Workbook
     End Function
 
     ' Worksheets
-    Public Function Workshts() As Worksheets
+    Public Function Worksheets() As Worksheets
     End Function
-    Public Function Workshts(name As String) As Worksheet
+    Public Function Worksheets(name As String) As Worksheet
     End Function
-    Public Function Workshts(index As Long) As Worksheet
+    Public Function Worksheets(index As Long) As Worksheet
     End Function
 
     Public Function Rows() As Range

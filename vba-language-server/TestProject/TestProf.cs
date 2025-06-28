93be10cb6e3b4ad0b2973dc41da7a8311be7fd66
y836475809 <tttdaytttday@gmail.com>
2025-06-28 22:28:01 +0900

    change: テスト整理

--- vba-language-server/TestProject/TestProf.cs
+++ vba-language-server/TestProject/TestProf.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
@@ -17,34 +18,35 @@ using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace TestProject {
-	public class TesProf(ITestOutputHelper output) {
+	public class TestProf(ITestOutputHelper output) {
 		private readonly ITestOutputHelper output = output;
-		[Fact]
-		public void TestProf1() {
-			var code = Helper.getCode("prof_sample1.bas");
-			var vbaca = new VBACodeAnalysis.VBACodeAnalysis();
-			var rewriter = new VBARewriter();
 
-			var sw = new System.Diagnostics.Stopwatch();
-			sw.Start();
-			var vbCode = rewriter.Rewrite("test", code);
-			vbaca.AddDocument("test", vbCode);
-			sw.Stop();
-			output.WriteLine($"{sw.ElapsedMilliseconds}ms");
+		[Fact]
+		public void TestProfRewriteAndAddDocument() {
+			var dirPath = GetDirPath();
+			var filePaths = Directory.GetFiles(dirPath, "*.bas");
+			foreach (var filePath in filePaths) {
+				var code = GetCode(filePath);
+				var vbaca = new VBACodeAnalysis.VBACodeAnalysis();
+				var rewriter = new VBARewriter();
+
+				var sw = new System.Diagnostics.Stopwatch();
+				sw.Start();
+				var vbCode = rewriter.Rewrite("test", code);
+				vbaca.AddDocument("test", vbCode);
+				sw.Stop();
+				var fileName = Path.GetFileName(filePath);
+				output.WriteLine($"{fileName} : {sw.ElapsedMilliseconds}ms");
+			}
 		}
 
-		[Fact]
-		public void TestProf2() {
-			var code = Helper.getCode("prof_sample2.bas");
-			var vbaca = new VBACodeAnalysis.VBACodeAnalysis();
-			var rewriter = new VBARewriter();
+		private static string GetCode(string filePath) {
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+			return Util.GetCode(filePath);
+		}
 
-			var sw = new System.Diagnostics.Stopwatch();
-			sw.Start();
-			var vbCode = rewriter.Rewrite("test", code);
-			vbaca.AddDocument("test", vbCode);
-			sw.Stop();
-			output.WriteLine($"{sw.ElapsedMilliseconds}ms");
+		private static string GetDirPath([CallerFilePath] string filePath = "") {
+			return Path.Combine(Path.GetDirectoryName(filePath), "prof_code");
 		}
 	}
 }
